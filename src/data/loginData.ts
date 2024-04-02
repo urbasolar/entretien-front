1286a209075ab590c315b37bb3570e99d33e4663
roman <cecile.roman@urbasolar.com>
2024-04-02 15:12:59 +0200

    Add additional login data fields

--- src/data/loginData.ts
+++ src/data/loginData.ts
@@ -1,4 +1,9 @@
 export const useLogin = {
     email: 'entretien@urbasolar.com',
     password: '123456',
+    job: 'Développeur',
+    company: 'Urbasolar',
+    city: 'Montpellier',
+    country: 'France',
+    phone: '0123456789',
 }
