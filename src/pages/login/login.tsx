d5e2542eb3b5bf45d31aab3874dcf324ffba9e27
roman <cecile.roman@urbasolar.com>
2024-04-02 12:30:37 +0200

    Add local storage functionality and handle login validation

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,12 +1,28 @@
 import { Button } from '@/components/button/button';
+import { useLogin } from '@/data/loginData';
 import { InputsLogin } from '@/modules/inputsLogin/inputsLogin';
+import { useState } from 'react';
 
 export const LoginPage = () => {
+  const [login, setLogin] = useState({
+    email: '',
+    password: '',
+  });
+  const [loginFailed, setLoginFailed] = useState(false);
+
+  const handleLogin = () => {
+    if (JSON.stringify(login) === JSON.stringify(useLogin)) {
+      console.log('login success');
+    } else {
+      setLoginFailed(true);
+    }
+  };
   return (
     <>
-      <InputsLogin />
-      <div className='mt-4 flex justify-center'>
-        <Button onClick={() => {}} text='login' size='md' />
+      <InputsLogin setData={setLogin} data={login} />
+      <div className='mt-4 flex justify-center flex-col items-center'>
+        <Button onClick={handleLogin} text='login' size='md' />
+        {loginFailed && <p className='text-red-500'>Login failed</p>}
       </div>
     </>
   );
