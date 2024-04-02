d278e2eb873fccaa168ca326afd71c4dc6520eae
roman <cecile.roman@urbasolar.com>
2024-04-02 14:59:33 +0200

    Add login functionality and redirect after successful login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,18 +1,31 @@
 import { Button } from '@/components/button/button';
 import { useLogin } from '@/data/loginData';
 import { InputsLogin } from '@/modules/inputsLogin/inputsLogin';
+import { saveDataLocalStorage } from '@/utils/localStorage';
+import { faSpinner } from '@fortawesome/pro-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginPage = () => {
+  const navigate = useNavigate();
   const [login, setLogin] = useState({
     email: '',
     password: '',
   });
   const [loginFailed, setLoginFailed] = useState(false);
+  const [loginSucceed, setLoginSucceed] = useState(false);
 
   const handleLogin = () => {
     if (JSON.stringify(login) === JSON.stringify(useLogin)) {
-      console.log('login success');
+      setLoginSucceed(true);
+      saveDataLocalStorage('user', login);
+      if (loginFailed) {
+        setLoginFailed(false);
+      }
+      setTimeout(() => {
+        navigate('/');
+      }, 2000);
     } else {
       setLoginFailed(true);
     }
@@ -21,8 +34,19 @@ export const LoginPage = () => {
     <>
       <InputsLogin setData={setLogin} data={login} />
       <div className='mt-4 flex justify-center flex-col items-center'>
-        <Button onClick={handleLogin} text='login' size='md' />
+        <Button
+          disabled={!login.email || !login.password}
+          onClick={handleLogin}
+          text='login'
+          size='md'
+        />
         {loginFailed && <p className='text-red-500'>Login failed</p>}
+        {loginSucceed && (
+          <div className='flex flex-col justify-center mt-m gap-m'>
+            <p className='text-green-500'>Login succeed</p>
+            <FontAwesomeIcon icon={faSpinner} spin />
+          </div>
+        )}
       </div>
     </>
   );
